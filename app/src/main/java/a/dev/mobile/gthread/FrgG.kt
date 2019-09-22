e789f64959a18f22ed6ca7fa6d1292fca4ed0ab1
trofimov <c.dev.mobile@gmail.com>
2019-09-22 12:48:00 +0300

    update

--- app/src/main/java/a/dev/mobile/gthread/FrgG.kt
+++ app/src/main/java/a/dev/mobile/gthread/FrgG.kt
@@ -1,7 +1,6 @@
-
-
 package a.dev.mobile.gthread
 
+import a.dev.mobile.gthread.HelpMy.arrayIntegerFromCursor
 import a.dev.mobile.gthread.HelpMy.arrayStringFromCursor
 import a.dev.mobile.gthread.databinding.RecyclerItemModelGBinding
 import android.content.Context
@@ -12,7 +11,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -73,6 +71,8 @@ class FrgG : Fragment() {
 
 
 
+        id = arrayIntegerFromCursor(cursor, DBColumn.ID)
+
         desc1 = arrayStringFromCursor(cursor, DBColumn.DESC1)
         threadPitch = arrayStringFromCursor(cursor, DBColumn.THREAD_PITCH)
 
@@ -80,6 +80,26 @@ class FrgG : Fragment() {
         threadPer = arrayStringFromCursor(cursor, DBColumn.THREAD_PER)
 
         class_ = arrayStringFromCursor(cursor, DBColumn.CLASS_)
+
+        exMajorDiaMax = arrayStringFromCursor(cursor, DBColumn.EX_MAJOR_DIA_MAX)
+        exMajorDiaMin = arrayStringFromCursor(cursor, DBColumn.EX_MAJOR_DIA_MIN)
+
+        exPitchDiaMax = arrayStringFromCursor(cursor, DBColumn.EX_PITCH_DIAM_MAX)
+        exPitchDiaMin = arrayStringFromCursor(cursor, DBColumn.EX_PITCH_DIAM_MIN)
+
+        exMinorDiaMax = arrayStringFromCursor(cursor, DBColumn.EX_MINOR_DIA_MAX)
+
+        inMajorDiaMin = arrayStringFromCursor(cursor, DBColumn.IN_MAJOR_DIA_MIN)
+
+        inMinorDiaMax = arrayStringFromCursor(cursor, DBColumn.IN_MINOR_DIA_MAX)
+        inMinorDiaMin = arrayStringFromCursor(cursor, DBColumn.IN_MINOR_DIA_MIN)
+
+        inPitchDiaMax = arrayStringFromCursor(cursor, DBColumn.IN_PITCH_DIAM_MAX)
+        inPitchDiaMin = arrayStringFromCursor(cursor, DBColumn.IN_PITCH_DIAM_MIN)
+
+        inTapDrill = arrayStringFromCursor(cursor, DBColumn.IN_TAP_DRILL)
+
+
 
 
 
@@ -93,11 +113,17 @@ class FrgG : Fragment() {
         val recycleView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recycleView.layoutManager = GridLayoutManager(activity, 1)
 
-
         //если хотим добавить разделители между ячейками
         //recycleView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
 
         recycleView.adapter = GListAdapter(activity)
+
+
+
+
+
+
+
         return view
     }
 
@@ -122,23 +148,23 @@ class FrgG : Fragment() {
             } else R.drawable.gaika
 
             val g = ModelG(
-                0,
+                id[position],
                 desc1[position],
                 desc2[position],
                 threadPitch[position],
                 threadPer[position],
                 class_[position],
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
-                "-",
+                exMajorDiaMax[position],
+                exMajorDiaMin[position],
+                exPitchDiaMax[position],
+                exPitchDiaMax[position],
+                exMinorDiaMax[position],
+                inMinorDiaMin[position],
+                inMinorDiaMax[position],
+                inPitchDiaMax[position],
+                inPitchDiaMin[position],
+                inMajorDiaMin[position],
+                inTapDrill[position],
                 image
             )
             holder.setData(g)
--- app/src/main/java/a/dev/mobile/gthread/FrgGDetails.kt
+++ app/src/main/java/a/dev/mobile/gthread/FrgGDetails.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
@@ -16,11 +17,15 @@ class FrgGDetails : Fragment() {
         private const val TAG = "== FrgGDetails"
         private const val G_MODEL = "model"
         private var isMM: Boolean = true
+        private var isEx: Boolean = true
         fun newInstance(gModel: ModelG, isMM: Boolean): FrgGDetails {
             val args = Bundle()
             args.putSerializable(G_MODEL, gModel)
             val fragment = FrgGDetails()
+
             this.isMM = isMM
+            isEx = gModel.class_ != ""
+
             fragment.arguments = args
             return fragment
         }
@@ -40,22 +45,43 @@ class FrgGDetails : Fragment() {
         val tvNameThreadMain: TextView = root.findViewById(R.id.tvNameThreadMain)
         val tvNameThreadSub: TextView = root.findViewById(R.id.tvNameThreadSub)
         val tvBaseDiamThread: TextView = root.findViewById(R.id.tvBaseDiam)
+        val tvTypeThread: TextView = root.findViewById(R.id.tvTypeThread)
+        val tvThreadPerInch: TextView = root.findViewById(R.id.tvThreadPerInch)
+        val tvThreadPith: TextView = root.findViewById(R.id.tvThreadPith)
+        val tvThreadClass: TextView = root.findViewById(R.id.tvThreadClass)
+        val viewEx1: LinearLayout = root.findViewById(R.id.viewEx1)
+        val tvTapDrill: TextView = root.findViewById(R.id.tvTapDrill)
+
+
+
+
 
         if (isMM) {
             tvNameThreadMain.text = "${model.desc1} x ${model.threadPitch}   ${model.class_}"
             tvNameThreadSub.text = "${model.desc2}\" - ${model.threadPer}"
-            tvBaseDiamThread.text =model.desc1.removePrefix("G ")
+            tvBaseDiamThread.text = model.desc1.removePrefix("G ") + " mm"
         } else {
             tvNameThreadMain.text = "${model.desc2}\" - ${model.threadPer}   ${model.class_}"
             tvNameThreadSub.text = "${model.desc1} x ${model.threadPitch}"
-            tvBaseDiamThread.text ="${model.desc2.removePrefix("G ")}\""
-
+            tvBaseDiamThread.text = "${model.desc2.removePrefix("G ")}\""
         }
 
 
+        if (isEx) tvTypeThread.text = "G - Трубная цилиндрическая наружняя"
+        else tvTypeThread.text = "G - Трубная цилиндрическая внутренняя"
 
+        tvThreadPerInch.text = model.threadPer
+        tvThreadPith.text = model.threadPitch + " mm"
 
+        if (isEx) tvThreadClass.text = model.class_
+        else tvThreadClass.text = "no"
 
+        if (isEx) viewEx1.visibility = View.GONE
+        else {
+            viewEx1.visibility = View.VISIBLE
+            if (isMM) tvTapDrill.text = model.inTapDrill + " mm"
+            else tvTapDrill.text = HelpMy.mmToInch(model.inTapDrill) + "\""
+        }
 
         return root
     }
